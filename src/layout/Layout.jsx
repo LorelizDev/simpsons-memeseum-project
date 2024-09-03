bd623b0938c5b71cf2c02f9001a4a3195c582e1d
LorelizDev <loreliz.dev@gmail.com>
2024-09-03 12:36:01 +0200

    my first commit

---
+++ src/layout/Layout.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+const Layout = () => {
+  return (
+    <>
+      <nav>
+        Mi NavBar
+      </nav>
+      <Outlet />
+      <footer>
+        Mi Footer
+      </footer>
+    </>
+  )
+}
+
+export default Layout
