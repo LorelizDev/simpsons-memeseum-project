762b0a757c9234fa1bfbe4353795926ac59ccbf8
juanvprada <juanvprada@gmail.com>
2024-09-03 21:36:06 +0200

    feat: add MemeView component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import MemeView from '../components/MemeView'
+
 
 const Home = () => {
   return (
     <div>
       <h1>Hola, soy Home👋</h1>
+      <MemeView />
     </div>
   )
 }
