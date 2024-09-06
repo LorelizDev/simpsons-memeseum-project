fb518cebcae0cd6ac808304a7b859489ac5a378c
LorelizDev <loreliz.dev@gmail.com>
2024-09-06 14:50:17 +0200

    refactor(navbar): create reusable image button component for navbar and other components style(index.html): apply background to root

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import ButtonAddMeme from './ButtonAddMeme'
+import ButtonImage from './ButtonImage'
 
 const Navbar = () => {
   return (
     <nav className='flex justify-between items-center h-36 bg-[url("src/assets/images/simpson_sky_background-removebg.png")]'>
       <div className='w-28 ml-3'>
-        <img src="src/assets/images/logo-simpson-removebg.png" alt="Logo Simpson's Memesuem"/>
+        <ButtonImage 
+          url_a='/'
+          url_image='src/assets/images/logo-simpson-removebg.png'
+          alt_img="Logo Simpson's Memesuem"
+        />
       </div>
       <div>
         <h1 className='font-simpson-title text-center text-6xl text-yellow-simp drop-shadow-simpson-title-ds'>Simpson's Memeseum</h1>
