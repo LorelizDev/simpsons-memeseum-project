e405687e72ac5d8e04f3225e6fe19f53e7b564d5
LorelizDev <loreliz.dev@gmail.com>
2024-09-06 15:18:01 +0200

    feat(ButtonImage): add target prop to <a> tag

--- src/components/ButtonImage.jsx
+++ src/components/ButtonImage.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const ButtonImage = ({url_a, url_image, alt_img}) => {
+const ButtonImage = ({url_a, target=null, url_image, alt_img}) => {
   return (
     <button type="button" className='h-full hover:scale-110 transition-all duration-300 ease-in-out'>
-        <a href={url_a} target="_blank" rel="noopener noreferrer">
+        <a href={url_a} target={target} rel="noopener noreferrer">
             <img src={url_image} alt={alt_img} className='h-full' />
         </a>
     </button>
