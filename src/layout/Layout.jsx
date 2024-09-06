36dc522cf17436a3cbfa3a30fa8baa3ea11d9ff6
LorelizDev <loreliz.dev@gmail.com>
2024-09-06 15:21:38 +0200

    feat: create Footer component

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from '../components/navbar'
+import Footer from '../components/Footer'
 
 const Layout = () => {
   return (
-    <body className='bg-gradient-to-b from-blue-simp from-0% via-[#486EDF] via-30% to-[#89C4FF] to-100%'>
+    <>
       <Navbar />
       <Outlet />
       
-      <footer>
-        Mi Footer
-      </footer>
-    </body>
+      <Footer />
+    </>
   )
 }
 
