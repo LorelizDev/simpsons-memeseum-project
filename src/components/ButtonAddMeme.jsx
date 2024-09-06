00dfff0dd872e4fac85e3d648bce17064a9da400
LorelizDev <loreliz.dev@gmail.com>
2024-09-06 12:12:51 +0200

    chore: Move images from 'img' to 'images' folder and delete 'img' folder

--- src/components/ButtonAddMeme.jsx
+++ src/components/ButtonAddMeme.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ButtonAddMeme = () => {
   return (
     <div className='w-32 mr-3 pt-3 flex self-start relative'>
-        <img src="src/assets/img/simpsons-doh.png" alt="Doh!" className='z-10' />
+        <img src="src/assets/images/simpsons-doh.png" alt="Doh!" className='z-10' />
         <button type="button" className='bg-brown-simp absolute w-full -bottom-5 -left-1 pt-3 pb-1 border border-black rounded-lg shadow-xl hover:bg-amber-800'><p className='font-simpson-title text-yellow-simp drop-shadow-simpson-outline'>Sube tu meme</p></button>
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,9 @@ import ButtonAddMeme from './ButtonAddMeme'
 
 const Navbar = () => {
   return (
-    <nav className='flex justify-between items-center h-36 bg-[url("src/assets/img/simpson_sky_background-removebg.png")]'>
+    <nav className='flex justify-between items-center h-36 bg-[url("src/assets/images/simpson_sky_background-removebg.png")]'>
       <div className='w-28 ml-3'>
-        <img src="src/assets/img/logo-simpson-removebg.png" alt="Logo Simpson's Memesuem"/>
+        <img src="src/assets/images/logo-simpson-removebg.png" alt="Logo Simpson's Memesuem"/>
       </div>
       <div>
         <h1 className='font-simpson-title text-center text-6xl text-yellow-simp drop-shadow-simpson-title-ds'>Simpson's Memeseum</h1>
