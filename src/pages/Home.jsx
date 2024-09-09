e51ad855af990784c5a95401f1d4ba823eee879c
LorelizDev <loreliz.dev@gmail.com>
2024-09-09 20:06:45 +0200

    feat(home): add image and button to enter to main gallery

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
-import MemeView from '../components/MemeView'
+import ButtonImage from '../components/ButtonImage'
+
 
 
 const Home = () => {
   return (
-    <div >
-      <h1>Hola, soy Home👋</h1>
-      <MemeView />
+    <div className='relative flex justify-center mx-10 h-[calc(100vh-9rem-5rem-5rem)]'> {/* 9rem mide el navbar, 5rem del footer, 2.5rem (x2) de margen de nav y footer */}
+      <img src="src/assets/images/front-museum.jpg" alt="Fachada de Simpson's Memeseum" className='max-h-full border-none rounded-3xl' />
+      <div className='absolute h-1/5 bottom-[32%] active:scale-90 active:hue-rotate-30'>
+        <ButtonImage 
+          url_a='main-gallery'
+          url_image="src/assets/images/buttton-enter.png"
+          alt_img="Botón para entrar a la galería"
+        />
+      </div>
     </div>
   )
 }
