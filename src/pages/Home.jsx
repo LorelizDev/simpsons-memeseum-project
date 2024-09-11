428bc1240f79655d50a20180da321113b954889c
LorelizDev <loreliz.dev@gmail.com>
2024-09-11 17:39:32 +0200

    refactor(home): removed hardcoded height calculation for full screen fit in Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import ButtonImage from '../components/ButtonImage'
 
 const Home = () => {
   return (
-    <div className='relative flex justify-center mx-10 h-[calc(100vh-9rem-5rem-5rem)]'> {/* 9rem mide el navbar, 5rem del footer, 2.5rem (x2) de margen de nav y footer */}
+    <div className='relative flex justify-center'>
       <img src="src/assets/images/front-museum.jpg" alt="Fachada de Simpson's Memeseum" className='max-h-full border-none rounded-3xl' />
       <div className='absolute h-1/5 bottom-[32%] active:scale-90 active:hue-rotate-30'>
         <ButtonImage 
