7d2a6af6d90e762c575e36e937c5938645623519
LorelizDev <loreliz.dev@gmail.com>
2024-09-18 10:20:44 +0200

    style(Instructions): change font type

--- src/pages/Instructions.jsx
+++ src/pages/Instructions.jsx
@@ -46,18 +46,18 @@ const instrucciones = [
 function UserManual() {
   return (
     <div className="p-4 bg-gray-100 rounded-lg shadow-md">
-      <h2 className="text-2xl font-bold mb-4">Instrucciones para Recorrer el Museo de Memes de los Simpsons</h2>
-      <p className="text-lg text-gray-700 mb-4">
+      <h2 className="text-2xl font-bold mb-4 font-simpson-title text-pink-simp">Instrucciones para Recorrer el Museo de Memes de los Simpsons</h2>
+      <p className="text-2xl text-gray-700 mb-4 font-simpson-p">
       Nuestro proyecto grupal consiste en el desarrollo de una aplicación CRUD (Crear, Leer, Actualizar, Borrar) dedicada a la recopilación y exhibición de memes de los Simpsons. Este museo virtual no solo celebra la rica historia de la serie animada, sino que también destaca su impacto cultural a través de los memes, que han trascendido generaciones y fronteras.
       </p>
       {instrucciones.map((item, index) => (
         <Disclosure key={index}>
           {({ open }) => (
             <>
-              <Disclosure.Button className={`w-full px-4 py-2 text-left text-lg font-medium text-gray-700 bg-blue-200 rounded-lg focus:outline-none focus-visible:ring focus-visible:ring-opacity-75 ${!open ? 'mb-4' : ''}`}>
+              <Disclosure.Button className={`w-full px-4 py-2 text-left text-lg font-bold font-simpson-title text-gray-700 bg-blue-200 rounded-lg focus:outline-none focus-visible:ring focus-visible:ring-opacity-75 ${!open ? 'mb-4' : ''}`}>
                 {item.titulo}
               </Disclosure.Button>
-              <Disclosure.Panel className="px-4 pt-4 pb-2 text-gray-700">
+              <Disclosure.Panel className="px-4 pt-4 pb-2 font-simpson-p text-xl text-gray-700">
                 <ul className="list-disc list-inside space-y-2">
                   {item.contenido.map((linea, idx) => (
                     <li key={idx}>{linea}</li>
