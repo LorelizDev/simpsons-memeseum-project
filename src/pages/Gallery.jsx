c8e78ab3b802e96c5cc6539789ba1034d089a69c
juanvprada <juanvprada@gmail.com>
2024-09-18 09:53:28 +0200

    refactor: remove redundant code initialImages styles

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -27,13 +27,13 @@ const Gallery = () => {
   const [showLargeImage, setShowLargeImage] = useState(false);
 
   const initialImages = [
-    { id: '1', image: initialImage1, top: '27%', left: '0%', width: '25%', height: '45%' },
-    { id: '2', image: initialImage2, top: '42%', left: '35%', width: '8%', height: '17%' },
-    { id: '4', image: initialImage4, top: '42%', left: '61.5%', width: '8%', height: '17%' },
-    { id: '6', image: initialImage6, top: '30%', left: '20%', width: '15%', height: '40%' },
-    { id: '7', image: initialImage7, top: '27%', left: '74%', width: '25%', height: '45%' },
-    { id: '8', image: initialImage8, top: '30%', left: '67%', width: '15%', height: '40%' },
-    { id: '9', image: initialImage9, top: '47%', left: '50.2%', width: '4.1%', height: '12%' },
+    { id: '1', image: initialImage1},
+    { id: '2', image: initialImage2},
+    { id: '4', image: initialImage4},
+    { id: '6', image: initialImage6},
+    { id: '7', image: initialImage7},
+    { id: '8', image: initialImage8},
+    { id: '9', image: initialImage9},
   ];
 
   const mainMemes = [meme1, meme2, meme4, meme6, meme7, meme8];
