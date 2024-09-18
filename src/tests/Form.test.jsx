e27b49094749aee511598221959e8edfe117bc05
LorelizDev <loreliz.dev@gmail.com>
2024-09-18 21:09:10 +0200

    refactor(Form.test): remove comments from code

--- src/tests/Form.test.jsx
+++ src/tests/Form.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Form from "../components/Form";
 import { MemoryRouter } from "react-router-dom";
 
@@ -32,49 +32,4 @@ describe("Form", () => {
 		const nameInput = screen.getByLabelText(/Nombra tu meme/i);
 		expect(nameInput).toBeDefined();
 	});
-
-	//   test('displays validation error when submitting empty name', async () => {
-	//     const submitButton = screen.getByRole('button', { name: /Crear Meme/i });
-
-	//     // Intenta enviar el formulario sin llenar el campo de nombre
-	//     fireEvent.click(submitButton);
-
-	//     // Verifica si se muestra el mensaje de error
-	//     const nameError = await screen.findByText(/El nombre es obligatorio/i);
-	//     expect(nameError).toBeDefined();
-	//   });
-
-	//   test('calls onSubmit when form is filled and submitted', async () => {
-	//     const nameInput = screen.getByLabelText(/Nombra tu meme/i);
-	//     const submitButton = screen.getByRole('button', { name: /Crear Meme/i });
-
-	//     // Llenar el campo de nombre
-	//     fireEvent.change(nameInput, { target: { value: 'Nuevo Meme' } });
-
-	//     // Simular la subida de una imagen
-	//     const imageUrlInput = screen.getByRole('textbox', { name: '' });
-	//     fireEvent.change(imageUrlInput, { target: { value: 'http://example.com/meme.jpg' } });
-
-	//     // Enviar el formulario
-	//     fireEvent.click(submitButton);
-
-	//     // Verifica si se ha llamado a la función onSubmit
-	//     expect(mockOnSubmit).toHaveBeenCalledWith({
-	//       name: 'Nuevo Meme',
-	//       image: 'http://example.com/meme.jpg',
-	//       like: 0,
-	//       dislike: 0,
-	//     });
-	//   });
-
-	//   test('displays image preview after image upload', () => {
-	//     const imageUrlInput = screen.getByRole('textbox', { name: '' });
-
-	//     // Simular la subida de una imagen
-	//     fireEvent.change(imageUrlInput, { target: { value: 'http://example.com/meme.jpg' } });
-
-	//     // Verificar si la imagen cargada se muestra en la vista previa
-	//     const imagePreview = screen.getByRole('img');
-	//     expect(imagePreview).toHaveAttribute('src', 'http://example.com/meme.jpg');
-	//   });
 });
