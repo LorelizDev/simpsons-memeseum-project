fc84b040d2cc269b0f2148a37c334c3799231017
LorelizDev <loreliz.dev@gmail.com>
2024-09-18 18:43:19 +0200

    refactor: remove unused useState import from Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
 import ButtonImage from "../components/ButtonImage";
 import AudioPlayer from "../components/AudioPlayer";
